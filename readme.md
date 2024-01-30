d3e05374abe447af0daf69febd757319916ed476
rtretter <25963632+rtretter@users.noreply.github.com>
2024-01-30 16:26:04 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,36 @@
+# Smart Paste
+This application allows pasting into any destination (even citrix). This is possible by sending the keystrokes in quick succession.
+After starting the application the hotkey "ctrl + shift + v" can be used to automatically paste the last entry in the clipboard.
+
+> :warning: **When copying multiple lines**: Return key is sent after every line, which can lead to issues in single-line inputs!
+
+## Installation
+To install the application it has to be built using cargo.
+1. Install Rust and Cargo via the official documentation (https://www.rust-lang.org/tools/install)
+2. Run `cargo build --release`
+3. The executable for your OS will be located in ./target/release/
+
+The application can be manually executed by executing the binary build or automatically started via the steps below.
+
+## MacOS Privacy Settings (Required)
+MacOS blocks external control of keystrokes via the settings. This app can be added to the whitelist in the settings.
+1. Open Settings > Privacy & Security > Accessibility
+2. Add Terminal (or smart-paste if running as Application)
+
+## MacOS Autostart (Optional)
+To automatically start the app on MacOS you have to create an application via the automator and then add it to the Login items.
+1. Press "Command + Space" - search for "automator"
+2. Choose Application
+3. Search for "shell script" and select "Run Shell Script"
+4. As command enter `/usr/local/bin/smart-paste`
+5. Press "Command + S" to save the app under the name `smart-paste`
+
+After creating the Application you now have to add it to the Login items:
+1. Open Settings > General > Login Items
+2. Click the "+" button
+3. Select the created app (smart-paste)
+
+## Windows Autostart (Optional)
+To add the app to autostart for windows it should be possible to simply add the application to the startup folder.
+1. Open the path `%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup`
+2. Paste the executable into this directory
---
+++ src/main.rs
@@ -0,0 +1,83 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use egui::IconData;
+use enigo::*;
+use global_hotkey::{GlobalHotKeyManager, hotkey::{HotKey, Modifiers, Code}, GlobalHotKeyEvent, HotKeyState};
+use arboard::Clipboard;
+use eframe::egui;
+use std::{thread, time::Duration};
+
+fn main() {
+    let manager = GlobalHotKeyManager::new().unwrap();
+    let hotkey_paste = HotKey::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyV);
+    manager.register(hotkey_paste).unwrap();
+    let global_hotkey_channel = GlobalHotKeyEvent::receiver();
+
+    std::thread::spawn(move || loop {
+            thread::sleep(Duration::from_millis(100));
+            if let Ok(event) = global_hotkey_channel.try_recv() {
+                if hotkey_paste.id() == event.id && event.state == HotKeyState::Released {
+                    let mut clipboard = Clipboard::new().unwrap();
+    
+                    let mut enigo = Enigo::new();
+                    enigo.key_click(Key::Control);
+                    thread::sleep(Duration::from_millis(50));
+                    let mut first = true;
+                    for line in clipboard.get_text().unwrap_or_else(|_| {
+                        println!("Clipboard is empty!");
+                        "".to_owned()
+                    }).split("\n") {
+                        if first {
+                            first = false;
+                        } else {
+                            enigo.key_click(Key::Return);
+                        }
+                        enigo.key_sequence(line);
+                    }
+                }
+            }
+    });
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_icon(load_icon()).with_decorations(false).with_transparent(true).with_mouse_passthrough(true),
+        ..Default::default()
+    };
+    let _ = eframe::run_native(
+        "smart-paste",
+        options,
+        Box::new(|_cc| Box::<SmartPaste>::default()),
+    );
+}
+
+fn load_icon() -> IconData {
+    let (icon_rgba, icon_width, icon_height) = {
+        let icon = include_bytes!("./../assets/icon.png");
+        let image = image::load_from_memory(icon)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+
+    IconData {
+        rgba: icon_rgba,
+        width: icon_width,
+        height: icon_height,
+    }
+}
+
+struct SmartPaste{}
+impl Default for SmartPaste {
+    fn default() -> Self {
+        Self{}
+    }
+}
+impl eframe::App for SmartPaste {
+    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+        egui::Rgba::TRANSPARENT.to_rgba_unmultiplied()
+    }
+
+    fn update(&mut self, _ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        
+    }
+}
